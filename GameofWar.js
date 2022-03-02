cd7f46e557838e929dbf32cee84b3d76d29c7c68
Evan Mei <em@Evans-MacBook-Pro.local>
2022-03-02 13:42:22 -0500

    added card and deck class

---
+++ GameofWar.js
@@ -0,0 +1,21 @@
+// created basic card class
+class Card {
+  constructor(suit, rank, score) {
+    this.suit = suit
+    this.rank = rank
+    this.score = score
+  }    
+}
+
+//create basic deck class
+class Deck {
+  constructor(name) {
+    this.name = name
+    this.deckLength = 52
+    this.cards = []
+    this.suits = ["Spades", "Hearts", "Clubs", "Diamonds"];
+    this.ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+    this.scores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+  }
+  }
